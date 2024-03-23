6984b919610e3341093f1e7392781d9a18b45c54
Nice <3078639052@qq.com>
2024-03-24 00:54:25 +0800

    webpack-builder testing

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    containercontainercontainercontainer
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.container {
+  color: blue;
+}
+</style>
